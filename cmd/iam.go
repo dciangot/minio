debf6c8db6c77b3cf348f07370b4dadb38086503
dciangot <diego.ciangottini@gmail.com>
2020-10-16 15:23:49 +0200

    iam.go

--- cmd/iam.go
+++ cmd/iam.go
@@ -1807,6 +1807,7 @@ func (sys *IAMSys) IsAllowedSTS(args iampolicy.Args) bool {
 // IsAllowed - checks given policy args is allowed to continue the Rest API.
 func (sys *IAMSys) IsAllowed(args iampolicy.Args) bool {
 	// If opa is configured, use OPA always.
+	logger.Info("globalPolicyOPA", globalPolicyOPA)
 	if globalPolicyOPA != nil {
 		ok, err := globalPolicyOPA.IsAllowed(args)
 		if err != nil {
